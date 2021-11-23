0637f79a49af538c2ebed0f03847a62ea99c8f48
Florian <hi@florianpanchout.com>
2021-11-23 15:28:12 +0100

    Login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+import queryString from 'query-string'
 
 export default function Login() {
-  return <div></div>
+  const location = useLocation()
+
+  const query = queryString.parse(location.search)
+
+  console.log(query)
+
+  return <div>Login</div>
 }
