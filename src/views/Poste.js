bc042e88e3ec964803abadb9b50fa35e76887601
Florian <hi@florianpanchout.com>
2021-12-09 07:37:29 +0100

    Ajout texte

--- src/views/Poste.js
+++ src/views/Poste.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { Row, Col, Breadcrumb, BreadcrumbItem } from '@dataesr/react-dsfr'
+import {
+  Row,
+  Col,
+  Breadcrumb,
+  BreadcrumbItem,
+  Title,
+  Text,
+} from '@dataesr/react-dsfr'
 
 import { useBilan } from 'hooks/useBilans'
 import { useEmissions } from 'hooks/useEmissions'
@@ -30,9 +37,14 @@ export default function Poste() {
       </Row>
       <Row gutters>
         <Col>
-          <h1>
+          <Title as='h1'>
             {bilan?.raisonSociale} - {bilan?.annee} - Poste {poste}
-          </h1>
+          </Title>
+          <Text>
+            La scope 1 concerne tous les gaz à effet de serre émis directement
+            par l'entreprise : le chauffage dans les locaux, les émissions des
+            véhicules détenus par l'entreprise, etc.
+          </Text>
         </Col>
       </Row>
       {emissions
