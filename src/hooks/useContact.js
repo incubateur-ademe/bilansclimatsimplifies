35ce81508ab49abcb46b8ab8362339c56e374423
Florian <hi@florianpanchout.com>
2022-02-22 19:18:44 +0100

    Fix form

--- src/hooks/useContact.js
+++ src/hooks/useContact.js
@@ -2,11 +2,5 @@ import { useMutation } from 'react-query'
 import axios from 'axios'
 
 export default function useContact() {
-  return useMutation((contact) =>
-    axios.post(`/`, contact, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    })
-  )
+  return useMutation((contact) => axios.post(`/`, contact))
 }
